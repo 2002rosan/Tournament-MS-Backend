9623f93cc4eb107d6ad5fce7e71eed980dde7fb0
Roshan Chaudhary <rosan.91111@gmail.com>
2024-01-20 10:23:04 +0545

    commit

---
+++ src/db/connection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const ConnectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `\nMongoDB connected! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MongoDB connection error: ${error}`);
+    process.exit(1);
+  }
+};
---
+++ src/utilities/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// Does the exact same work as above
+// const asyncHandler = async (fn) => {
+//   (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//       res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message,
+//       });
+//     }
+//   };
+// };
