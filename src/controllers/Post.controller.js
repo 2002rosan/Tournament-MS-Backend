025be57176bd267bb250e4f94fae818bc6d99918
Roshan Chaudhary <rosan.91111@gmail.com>
2024-02-08 14:57:02 +0545

    Added get user's post by id features

--- src/controllers/Post.controller.js
+++ src/controllers/Post.controller.js
@@ -3,6 +3,7 @@ import { apiError } from "../utils/apiError";
 import { apiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 
+// To create post / tweet
 const createPost = asyncHandler(async (req, res) => {
   const { postData } = req.body;
   const userId = req.user?._id;
@@ -16,4 +17,37 @@ const createPost = asyncHandler(async (req, res) => {
   if (!data) throw new apiError(500, "Server Error");
 
   return res.status(200).json(new apiResponse(200, data, "Post tweeted"));
+});
+
+// Get all posts of a specific user
+const getUserPosts = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+
+  const data = await Post.aggregate([
+    {
+      $match: {
+        owner: userId,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "userPosts",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        content: 1,
+        "userPosts.userName": 1,
+      },
+    },
+  ]);
+  if (!data) throw new apiError(500, "Error fetching posts");
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, data, "Posts fetched successfully"));
 });
