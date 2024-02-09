86d0ad9c79bb7b10fcd27f84e8d3d4d054e944ff
Roshan Chaudhary <rosan.91111@gmail.com>
2024-02-09 12:12:16 +0545

    Completed Like controller

--- src/controllers/Like.controller.js
+++ src/controllers/Like.controller.js
@@ -5,6 +5,7 @@ import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { Comment } from "../models/comment.model.js";
+import { Post } from "../models/post.model.js";
 
 // For video likes
 const toggleVideoLike = asyncHandler(async (req, res) => {
@@ -68,3 +69,81 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
       .json(new apiResponse(400, {}, "You already like this comment"));
   }
 });
+
+// For Post Likes
+const togglePostLike = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const { postId } = req.params;
+
+  const checkId = await Post.findById(postId);
+  if (!checkId) throw new apiError(404, "Invalid post ID");
+
+  const checkData = await Like.aggregate([
+    {
+      $match: {
+        post: new mongoose.Types.ObjectId(checkId),
+        likedBy: new mongoose.Types.ObjectId(userId),
+      },
+    },
+  ]);
+
+  if (checkData.length == 0) {
+    const toggleLike = await Like.create({
+      likedBy: userId,
+      post: postId,
+    });
+    await toggleLike.save();
+
+    return res.status(200).json(new apiResponse(200, {}, "Post liked"));
+  } else {
+    return res
+      .status(400)
+      .json(new apiResponse(400, {}, "Already liked the post"));
+  }
+});
+
+// For total likes in a video
+const totalLikesInVideo = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+
+  const likedVideoList = await Like.aggregate([
+    {
+      $match: {
+        likedBy: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "data",
+      },
+    },
+    {
+      $unwind: "$data",
+    },
+    {
+      $project: {
+        "data.videoFile": 1,
+        "data.thumbNail": 1,
+        "data.owner": 1,
+        "data.title": 1,
+        "data.description": 1,
+        "data.duration": 1,
+      },
+    },
+  ]);
+  if (!likedVideoList) throw new apiError(500, "Server Error");
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, likedVideoList, "liked video lists"));
+});
+
+export {
+  toggleVideoLike,
+  toggleCommentLike,
+  togglePostLike,
+  totalLikesInVideo,
+};
